fb653b76efa2760c7badb68c7cd41cefbbb94c63
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-04-23 23:07:34 +0200

    2 Script

---
+++ PDFMerger/PdfMerge.py
@@ -0,0 +1,13 @@
+#PDF merge
+# A program a PyPDF2 modul segítségével két pdf fájlt egyesít egy fájlba.
+
+import PyPDF2
+import os
+import sys
+
+merger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        merger.append(file)
+merger.write("merged.pdf")
