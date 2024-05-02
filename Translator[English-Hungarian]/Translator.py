03ca6ea903095358a1b995d8c8b3b5b9b27350c5
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-05-02 10:04:39 +0200

    Base

--- Translator[English-Hungarian]/Translator.py
+++ Translator[English-Hungarian]/Translator.py
@@ -10,11 +10,24 @@ def main():
         text = input("Enter the text you want to translate: ")
         if text == "exit":
             break
-        direction = input("Enter 'en-hu' for English to Hungarian or 'hu-en' for Hungarian to English translation: ")
+        direction = input("Enter the direction of the translation (en-hu, hu-en, en-de, de-en, hu-de, de-hu, hu-fr, fr-hu): ")
         if direction == 'en-hu':
             translation = translator.translate(text, src='en', dest='hu')
         elif direction == 'hu-en':
             translation = translator.translate(text, src='hu', dest='en')
+        elif direction == 'en-de':
+            translation = translator.translate(text, src='en', dest='de')
+        elif direction == 'de-en':
+            translation = translator.translate(text, src='de', dest='en')
+        elif direction == 'hu-de':
+            translation = translator.translate(text, src='hu', dest='de')
+        elif direction == 'de-hu':
+            translation = translator.translate(text, src='de', dest='hu')
+        elif direction == 'hu-fr':
+            translation = translator.translate(text, src='hu', dest='fr')
+        elif direction == 'fr-hu':
+            translation = translator.translate(text, src='fr', dest='hu')
+
         else:
             print("Invalid direction! Please enter 'en-hu' or 'hu-en'.")
             continue
